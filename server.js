0cb7b6df3d66c0adb58a5b5e5986f74dab1d8044
omickelsen <omickelsen@gmail.com>
2019-09-30 21:33:17 -0600

    string to newchar

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-let csvResult= "";
+app.use(bodyParser.urlencoded({ extended: false }));
+let csvResult = "";
 app.set('Content-Type', 'text/plain');
-app.post('/formatCSV', function(req, res ) {
+app.post('/formatCSV', function (req, res) {
 
 
-csvResult = formatCSV(req.body.csv);
-// console.log(req.body.csv);
-res.send(csvResult);
+  csvResult = formatCSV(req.body.csv);
+  // console.log(req.body.csv);
+  res.send(csvResult);
 
 });
 
-
+let finishedOutput = "";
+let finishedString = "";
 
 
 
@@ -24,38 +25,90 @@ function formatCSV(str) {
   if (!str) {     // Verifies that str exists
     return "Input was null";
   }
- 
+
   let lines = [];
-  lines = str.split('\n');
-  console.log(lines[0]);
-  console.log("this is after");
-
-  let headerFound = false;
-  lines.forEach(function(line){
-   let formattedHeader = ""
-    if(!headerFound){
-      let parts = [];
-      parts = line.split(",")
-      parts.forEach(function(part){
-        formattedHeader += '[ ' + part + ' ]'  
-
-      })
-      headerFound = true;
-      
+  lines = str.split(/\r\n|\n/);
+  // console.log(lines[0]);
+  // console.log("this is after");
+  for ( let i = 0; i < lines.length; i++) {
+    // console.log(lines.length, " HOW MANY TIMES AM I RUNNING");
+    line = lines[i];
+    if (line.length == 0){
+      break;
     }
-    else{
-      // turn each line into a character array "pieces"
-
-
-      //if first character in pieces is quote, read until find next quote
-      //else read until you find next comma
-
-
-
+    // line = line.toString().replace('"', '&');
+    // console.log(line, " REPLACED STRING ");
+    finishedString = "";
+    let pieces = [];
+    pieces = line.split("");
+    console.log(pieces, " PIECES HERE");
+    startsWithQuote = false;
+    let tempX = "";
+    console.log( pieces[0], " here is pieces at index 0");
+    if (pieces[0] === '"') {
+      startsWithQuote = true;
+      console.log(startsWithQuote, " starting with starts with quote.");
     }
-  })
+    let parcedStr = "";
+    startNewPiece = false;
+    // console.log(pieces.length, " Pieces lengthy pieces")
+    for (let i = 1; i < pieces.length; i++) {
+      // console.log(i, " what am I doing?????");
+      console.log(startsWithQuote, " why not made it to starts with quote");
+      if (startsWithQuote) {
+        console.log(" made it to starts with quote");
+        if (startNewPiece == true) {
+          if (pieces[i] == '"') {
+            startNewPiece = false;
+          }
+        } else if (pieces[i] != '"'){
+          tempX += pieces [i];
+          console.log(tempX," TEMPX");
+        } else {
+          console.log(" made it to after TempX");
+          parcedStr += "[" + tempX + "]";
+          finishedString += parcedStr;
+          parcedStr = "";
+          tempx = "";
+          startNewPiece = true;
+
+          if ( i + 2 < pieces.length && pieces [i + 2] != '"'){
+            startsWithQuote = false;
+          }
+        }
+      } else {
+        console.log("do nothing if its comma");
+        if (pieces [i] == ',' && tempX.length == 0){
+            //do nothing if character is a comma
+        }
+        else if (pieces[i] != ','){
+          console.log(" here is a replacement");
+          tempX += pieces[i];
+        }
+        else{
+          parcedStr += "[" + tempX + "]";
+          finishedString += parcedStr;
+          parcedStr = "";
+          tempx = "";
+          startNewPiece = true;
+          if (i + 1 < pieces.length && pieces [i + 1] == '"'){
+            startsWithQuote = true;
+          }
+
+        }
+
+      }
+      if (finishedOutput.length > 0){
+        finishedOutput += "\r\n";
+      }
+      finishedOutput += finishedString;
+    }
+    
+    // console.log(finishedOutput, " is this finished output?");
+    return finishedOutput;
+  }
+
 }
-  
 
 
-app.listen( PORT, () => console.log( `Server started on port ${PORT}` ));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
